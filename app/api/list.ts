159c50fd1d6552e17b6da04a0b02b4546098378b
emmachase <me@its-em.ma>
2025-03-23 15:35:58 -0700

    refactor: improve input validation and query logic in list API

--- app/api/list.ts
+++ app/api/list.ts
@@ -1,43 +1,48 @@
 import { authedProcedure } from "./trpc";
 import { db } from "~/db";
 import { upload, tag } from "~/db/schema";
-import { desc, eq, like, lt, or, and } from "drizzle-orm";
+import { desc, eq, like, or, and, lte } from "drizzle-orm";
 import * as v from "valibot";
 
 const DEFAULT_LIMIT = 30;
 
 export const list = authedProcedure("list")
   .input(
-    v.object({
-      cursor: v.optional(v.number()),
-      limit: v.optional(v.number()),
-      mfilter: v.optional(v.string()),
-      gallery: v.optional(v.boolean()),
-    }),
+    v.intersect([
+      v.union([
+        v.object({
+          mfilter: v.optional(v.string()),
+        }),
+        v.object({
+          gallery: v.optional(v.boolean()),
+        }),
+      ]),
+      v.object({
+        cursor: v.optional(v.number()),
+        limit: v.optional(v.number()),
+      }),
+    ]),
   )
   .query(async ({ input }) => {
     const limit = input.limit ?? DEFAULT_LIMIT;
-    const { cursor, mfilter, gallery } = input;
+    const { cursor } = input;
 
-    const where = gallery
-      ? or(like(upload.mime, "%image%"), like(upload.mime, "%video%"))
-      : mfilter
-        ? like(upload.mime, `%${mfilter}%`)
-        : undefined;
+    const whereCursor = cursor ? lte(upload.id, cursor) : undefined;
+
+    const where =
+      "gallery" in input
+        ? or(like(upload.mime, "%image%"), like(upload.mime, "%video%"))
+        : "mfilter" in input
+          ? like(upload.mime, `%${input.mfilter}%`)
+          : undefined;
 
     const results = await db
       .select()
       .from(upload)
       .leftJoin(tag, eq(upload.id, tag.uploadId))
-      .where(
-        cursor && where
-          ? and(lt(upload.id, cursor), where)
-          : cursor
-            ? lt(upload.id, cursor)
-            : where,
-      )
+      .where(and(whereCursor, where))
       .orderBy(desc(upload.id))
-      .limit(limit);
+      .limit(limit + 1); // +1 to check if there are more results
 
     // Group tags by upload
     const uploadsWithTags = results.reduce<
